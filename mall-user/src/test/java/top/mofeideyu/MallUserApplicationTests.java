08aa98e000b67a4a97af3174204067cd0d86375f
mofeideyu <mofei2914376031@163.com>
2023-06-06 16:13:45 +0800

    微服务集成nacos服务注册中心(剩余文件)

---
+++ mall-user/src/test/java/top/mofeideyu/MallUserApplicationTests.java
@@ -0,0 +1,12 @@
+package top.mofeideyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallUserApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
